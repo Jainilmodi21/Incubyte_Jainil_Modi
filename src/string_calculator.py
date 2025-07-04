c75f171a0f5ae39ab0d97ffb8447538a6099b807
Jainilmodi21 <modi.jainil2110@gmail.com>
2025-07-04 21:25:23 +0530

    test for string containing 1 and 2 number

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,4 +1,4 @@
 def add(numbers: str) -> int:
-    if numbers == "":
+    if numbers== "":
         return 0
     
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -2,8 +2,13 @@ import unittest
 from src.string_calculator import add
 
 class TestStringCalculator(unittest.TestCase):
-    def test_empty_string_returns_zero(self):
+    def test_string_calculator(self):
+        #test for empty string
         self.assertEqual(add(""), 0)
+
+        #test for 1 and 2 numbers
+        self.assertEqual(add("1"), 1)
+        self.assertEqual(add("1,2"), 3)
 
 if __name__ == '__main__':
     unittest.main()
