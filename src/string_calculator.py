c320c16bb252d09e6a7488092ba73bed3a447306
Jainilmodi21 <modi.jainil2110@gmail.com>
2025-07-04 21:29:50 +0530

    implement add method for string containing 1 and 2 number

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -2,3 +2,8 @@ def add(numbers: str) -> int:
     if numbers== "":
         return 0
     
+    #split string in numbers using "," delimeter
+    parts = numbers.split(",")
+    # convert to ints and sum
+    return sum(int(n) for n in parts)
+    
