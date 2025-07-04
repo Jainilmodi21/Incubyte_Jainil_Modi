6a5a139eaaea557089af0396f7d2a7902ea597d4
Jainilmodi21 <modi.jainil2110@gmail.com>
2025-07-04 21:38:12 +0530

    test for \n delimeter

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -2,6 +2,7 @@ def add(numbers: str) -> int:
     if numbers== "":
         return 0
     
+
     #split string in numbers using "," delimeter
     parts = numbers.split(",")
     # convert to ints and sum
