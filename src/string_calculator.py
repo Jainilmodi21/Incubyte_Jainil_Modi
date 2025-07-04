65c012ea6fd90d110b04e9e216440875b2ad2912
Jainilmodi21 <modi.jainil2110@gmail.com>
2025-07-04 23:35:53 +0530

    implement get_called_count method

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,38 +1,48 @@
-def add(numbers: str) -> int:
-    if numbers== "":
-        return 0
-    
-    delim = ","
-    # check for custom delimiter syntax 
-    if numbers.startswith("//"):
-        # split into header and the rest
-        header, numbers = numbers.split("\n", 1)
-        delim = header[2:]      
 
-    # normalize newlines to the chosen delimiter
-    numbers = numbers.replace("\n", delim)
+class StringCalculator:
+    def __init__(self):
+        self._call_count = 0
 
-    parts = numbers.split(delim)
 
-    
-    negatives = []
-    for part in parts:
-        if not part:
-            continue
+    def add(self,numbers: str) -> int:
+        self._call_count += 1
+        if numbers== "":
+            return 0
+        
+        delim = ","
+        # check for custom delimiter syntax 
+        if numbers.startswith("//"):
+            # split into header and the rest
+            header, numbers = numbers.split("\n", 1)
+            delim = header[2:]      
 
-        number = int(part)
+        # normalize newlines to the chosen delimiter
+        numbers = numbers.replace("\n", delim)
 
-        # if it’s negative, store it
-        if number < 0:
-            negatives.append(number)
+        parts = numbers.split(delim)
 
-    if negatives:
-        # list all negatives in the message
-        negative_str = ", ".join(str(n) for n in negatives)
-        raise ValueError(f"negatives not allowed: {negative_str}")
+        
+        negatives = []
+        for part in parts:
+            if not part:
+                continue
 
-    if any(part == "" for part in parts):
-        raise ValueError("invalid input")
-    # convert to ints and sum
-    return sum(int(n) for n in parts)
-    
+            number = int(part)
+
+            # if it’s negative, store it
+            if number < 0:
+                negatives.append(number)
+
+        if negatives:
+            # list all negatives in the message
+            negative_str = ", ".join(str(n) for n in negatives)
+            raise ValueError(f"negatives not allowed: {negative_str}")
+
+        if any(part == "" for part in parts):
+            raise ValueError("invalid input")
+        # convert to ints and sum
+        return sum(int(n) for n in parts)
+        
+
+    def get_called_count(self) -> int:
+        return self._call_count
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,30 +1,37 @@
 import unittest
-from src.string_calculator import add
+from src.string_calculator import StringCalculator
 
 class TestStringCalculator(unittest.TestCase):
     def test_string_calculator(self):
+
+        calc = StringCalculator()
         #test for empty string
-        self.assertEqual(add(""), 0)
+        self.assertEqual(calc.add(""), 0)
 
         #test for 1 and 2 numbers
-        self.assertEqual(add("1"), 1)
-        self.assertEqual(add("1,2"), 3)
+        self.assertEqual(calc.add("1"), 1)
+        self.assertEqual(calc.add("1,2"), 3)
 
         #test for \n delimeter
-        self.assertEqual(add("1\n2,3"), 6)
+        self.assertEqual(calc.add("1\n2,3"), 6)
 
         #for invalid input
         with self.assertRaises(ValueError) as ctx:
-            add("1,\n")
+            calc.add("1,\n")
             self.assertEqual(str(ctx.exception), "invalid input")
 
         #for custom delimeter
-        self.assertEqual(add("//|\n4|5|6"), 15)
+        self.assertEqual(calc.add("//|\n4|5|6"), 15)
 
         #for negative numbers
         with self.assertRaises(ValueError) as ctx:
-            add("1,-2,3")
+            calc.add("1,-2,3")
         self.assertEqual(str(ctx.exception), "negatives not allowed: -2")
+
+
+        #count the number of add calls
+        self.assertEqual(calc.get_called_count(), 7)
+
 
        
 
